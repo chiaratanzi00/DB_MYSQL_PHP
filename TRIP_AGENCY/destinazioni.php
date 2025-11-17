644552d707fa7eb4b2f276dff12184261c05ae91
chiaratanzi00 <chiaratanzi00@gmail.com>
2025-11-17 18:46:40 +0100

    corretti piccoli errori di scrittura

--- TRIP_AGENCY/destinazioni.php
+++ TRIP_AGENCY/destinazioni.php
@@ -1,5 +1,92 @@
 <?php include 'header.php'; ?>
-<?php include 'db.php'; ?>
+<?php include 'db.php'; 
+
+//Logica per impaginazione
+$perPagina = 10;  // n elementi mostrati per pagina
+$page = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+$offset = ($page - 1) * $perPagina;
+
+
+//===========================  LOGICA DI AGGIUNTA DESTINAZIONE
+//chiamata POST che prende il gancio del bottone aggiugi del form, prendendo i valori inseriti nei vari campi
+if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['aggiungi'])){
+
+//Preparo lo stato stmt -> statement 
+$stmt = $conn->prepare("INSERT INTO destinazioni 
+        (citta, paese, prezzo, data_partenza, data_ritorno, posti_disponibili)
+        VALUES (?, ?, ?, ?, ?, ?)
+");
+
+//Binding dei parametri e tipizzo
+$stmt->bind_param("ssssss",
+        $_POST['citta'],
+        $_POST['paese'],
+        $_POST['prezzo'],
+        $_POST['data_partenza'],
+        $_POST['data_ritorno'],
+        $_POST['posti_disponibili']
+    );
+
+    $stmt->execute();
+
+    echo "<div class='alert alert-success'>Destinazione aggiunta con successo!</div>";
+}
+
+
+
+// =========================== LOGICA DI MODIFICA
+$destinazione_modifica = null;
+
+if (isset($_GET['modifica'])) {
+
+    $id = intval($_GET['modifica']);
+    $res = $conn->query("SELECT * FROM destinazioni WHERE id = $id");
+
+    $destinazione_modifica = $res->fetch_assoc();
+}
+
+
+// ============================== SALVATAGGIO MODIFICA
+if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['salva_modifica'])){
+
+//PREPARE
+$stmt = $conn->prepare("UPDATE destinazioni
+        SET citta=?, paese=?, prezzo=?, data_partenza=?, data_ritorno=?, posti_disponibili=?
+        WHERE id=?"
+);
+
+//BINDING
+$stmt->bind_param("ssssssi",
+        $_POST['citta'],
+        $_POST['paese'],
+        $_POST['prezzo'],
+        $_POST['data_partenza'],
+        $_POST['data_ritorno'],
+        $_POST['posti_disponibili'],
+        $_POST['id']
+);
+
+//ESECUZIONE QUERY
+$stmt->execute();
+
+//messaggio
+echo "<div class='alert alert-info'>Destinazione modificata correttamente!</div>";
+}
+
+// ============================== CANCELLAZIONE DESTINAZIONE
+if(isset($_GET['elimina'])){
+
+$id = intval($_GET['elimina']);
+$stmt = $conn->prepare("DELETE FROM destinazioni WHERE id=?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+echo "<div class='alert alert-info'>Destinazione eliminata correttamente</div>";
+}
+    
+?>
+
+
 
 <h2>Destinazioni</h2>
 
@@ -10,47 +97,81 @@
 
             <form action="" method="POST">
 
+            <?php if($destinazione_modifica): ?>
+                
+                    <input type="hidden" name="id" value="<?= $destinazione_modifica['id'] ?>">
+
+                <?php endif; ?>
+
+
                 <div class="row g-3">
                     
                     <div class="col-md-6">
                         <label style="font-weight: 600;" for="">Città : </label>
-                        <input type="text" name="citta" class="form-control" placeholder="es.: Milano" required>
+                        <input type="text" name="citta" class="form-control" placeholder="es.: Milano"
+                        value="<?= $destinazione_modifica['citta'] ?? ''?>"
+                        required>
                     </div>
                     
                     <div class="col-md-6">
                         <label style="font-weight: 600;" for="">Paese : </label>
-                        <input type="text" name="paese" class="form-control" placeholder="es.: Italia" required>
+                        <input type="text" name="paese" class="form-control" placeholder="es.: Italia" 
+                        value="<?= $destinazione_modifica['paese'] ?? ''?>"
+                        required>
                     </div>
                     
                     <div class="col-md-6">
                         <label style="font-weight: 600;" for="">Prezzo : </label>
-                        <input type="number" name="prezzo" class="form-control" placeholder="" required>
+                        <input type="number" name="prezzo" class="form-control" placeholder=""
+                        value="<?= $destinazione_modifica['prezzo'] ?? ''?>" required>
                     </div>
                     
                     <div class="col-md-6">
                         <label style="font-weight: 600;" for="">Data Partenza : </label>
-                        <input type="date" name="data_partenza" class="form-control" placeholder="" required>
+                        <input type="date" name="data_partenza" class="form-control" placeholder=""
+                        value="<?= $destinazione_modifica['data_partenza'] ?? ''?>" required>
                     </div>
                     
                     <div class="col-md-6">
                         <label style="font-weight: 600;" for="">Data Ritorno : </label>
-                        <input type="date" name="data_ritorno" class="form-control" placeholder="" required>
+                        <input type="date" name="data_ritorno" class="form-control" placeholder=""
+                        value="<?= $destinazione_modifica['data_ritorno'] ?? ''?>" required>
                     </div>
                     
                     <div class="col-md-6">
                         <label style="font-weight: 600;" for="">Posti Disponibili : </label>
-                        <input type="number" name="posti_disponibili" class="form-control" placeholder="" required>
+                        <input type="number" name="posti_disponibili" class="form-control" placeholder="" 
+                        value="<?= $destinazione_modifica['posti_disponibili'] ?? ''?>"required>
                     </div>
                     
                     
                     <div class="col-12">
-                        <button class="btn btn-success" type="submit">Salva</button>
+                        <button name="<?= $destinazione_modifica ? 'salva_modifica' : 'aggiungi' ?>" class="<?= $destinazione_modifica ? 'warning' : 'success' ?>" 
+                        type="submit">
+                        <?= $destinazione_modifica? 'Salva' : 'Aggiungi' ?>
+                        </button>
                     </div>
 
                 </div>
             </form>
         </div>
     </div>
+
+
+
+
+    <!-- ============================== LOGICA RENDER -->
+    <?php
+
+        //vado a conteggiare il totale dei clienti con query
+        $total = $conn->query("SELECT COUNT(*) as t FROM destinazioni")->fetch_assoc()['t'];
+        $totalPages = ceil($total / $perPagina); // il numero di pagine della navigazione
+
+        //QUERY PER ordinare i dati in modo DECRESCENTE IMPAGINATI PER valore di "$perPagina" 
+        $result = $conn->query("SELECT * FROM destinazioni ORDER BY id ASC LIMIT $perPagina OFFSET $offset");
+
+    ?>
+
 
 
     <!--Tabella-->
@@ -73,14 +194,48 @@
 
         </thead>
 
+
         <tbody>
 
+        <?php while ($row = $result->fetch_assoc()) : ?>
+                
+                <tr>
+                <td><?= $row['id'] ?></td>
+                <td><?= $row['citta'] ?></td>
+                <td><?= $row['paese'] ?></td>
+                <td><?= $row['prezzo'] ?></td>
+                <td><?= $row['data_partenza'] ?></td>
+                <td><?= $row['data_ritorno'] ?></td>
+                <td><?= $row['posti_disponibili'] ?></td>
 
-        
+                <td>
+
+                    <a class="btn btn-sm btn-warning" href="?modifica=<?= $row['id'] ?>">Modifica</a>
+                    <a class="btn btn-sm btn-danger" href="?elimina=<?= $row['id'] ?>"
+                        onclick="return confirm('Sei sicuro di voler eliminare?');">
+                        Elimina
+                    </a>
+                
+                </td>
+
+                </tr>
+
+        <?php endwhile; ?>
 
         </tbody>
 
     </table>
 
 
-<?php include 'footer.php'; ?>
+    <!-- PAGINAZIONE -->
+    <nav>
+    <ul class="pagination">
+        <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+            <li class="page-item <?= ($i == $page) ? 'active' : '' ?>">
+                <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+            </li>
+        <?php endfor; ?>
+    </ul>
+    </nav>
+
+    <?php include 'footer.php'; ?>
