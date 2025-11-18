797abcd22d3b7794ed011128cbc7b1462f484990
chiaratanzi00 <chiaratanzi00@gmail.com>
2025-11-18 15:34:04 +0100

    modifiche

--- TRIP_AGENCY/prenotazioni.php
+++ TRIP_AGENCY/prenotazioni.php
@@ -13,6 +13,7 @@
     $clienti = $conn->query("SELECT id, nome, cognome FROM clienti");
     $destinazioni = $conn->query("SELECT id, citta, paese FROM destinazioni");
 
+    
 
 
     //LOGICA DI AGGIUNTA
@@ -35,8 +36,6 @@
 
 
 
-
-
     //LOGICA DI MODIFICA
     $prenotazione_modifica = null;
 
@@ -48,10 +47,6 @@
         $prenotazione_modifica = $res->fetch_assoc();
 
     }
-
-
-
-
 
     //MODIFICA DEL DATO, SALVATAGGIO 
     if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['salva_modifica'])){
@@ -67,10 +62,7 @@
     }
 
 
-
-
-
-    //CANCELLAZIONE CLIENTE
+//CANCELLAZIONE CLIENTE
     if(isset($_GET['elimina'])){
 
         $id = intval($_GET['elimina']);
@@ -79,14 +71,31 @@
         echo "<div class='alert alert-info'>Prenotazione Cancellata correttamente</div>";
     }
 
+    //vado a conteggiare il totale dei clienti con query
+    $total = $conn->query("SELECT COUNT(*) as t FROM prenotazioni")->fetch_assoc()['t'];
+    $totalPages = ceil($total / $perPagina); // il numero di pagine della navigazione
+
+    //QUERY PER ordinare i dati in modo DECRESCENTE IMPAGINATI PER valore di "$perPagina" 
+    //$result = $conn->query("SELECT * FROM prenotazioni ORDER BY id ASC LIMIT $perPagina OFFSET $offset");
+
+
+    //QUERY ASSOCIAZIONE JOIN TRA LE DUE TABELLE 
+
+    $stmt = $conn->prepare("SELECT p.id, c.nome, c.cognome, d.citta, d.paese, p.dataprenotazione, p.numero_persone, p.acconto, p.assicurazione
+                            FROM prenotazioni p
+                            JOIN clienti c ON p.id_cliente = c.id
+                            JOIN destinazioni d ON p.id_destinazione = d.id
+                            ORDER BY p.id DESC LIMIT ? OFFSET ?
+                            ");
+    $stmt->bind_param("ii", $perPagina, $offset);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+
     
  ?>
 
-
-
-
-
-<h2>Prenotazioni</h2>
+ <h2>Prenotazioni</h2>
 
     <!--Form-->
     <div class="card mb-4">
@@ -198,16 +207,9 @@
     <!--LOGICA RENDER -->
     <?php
 
-        //vado a conteggiare il totale dei clienti con query
-        $total = $conn->query("SELECT COUNT(*) as t FROM prenotazioni")->fetch_assoc()['t'];
-        $totalPages = ceil($total / $perPagina); // il numero di pagine della navigazione
-
-        //QUERY PER ordinare i dati in modo DECRESCENTE IMPAGINATI PER valore di "$perPagina" 
-        $result = $conn->query("SELECT * FROM prenotazioni ORDER BY id ASC LIMIT $perPagina OFFSET $offset");
+  
 
     ?>
-
-
 
 
 
@@ -237,7 +239,7 @@
                 
                 <tr>
                     <td><?= $row['id'] ?></td>
-                    <td><?= $row['nome'] ?><?= $row['cognome'] ?></td>
+                    <td><?= $row['nome'] . ' ' . $row['cognome'] ?></td>
                     <td><?= $row['citta'] ?></td>
                     <td><?= $row['dataprenotazione'] ?></td>
                     <td><?= $row['acconto'] ?></td>
