283889be0827134581a470da128af7b5b9652bd6
chiaratanzi00 <chiaratanzi00@gmail.com>
2025-11-18 12:33:50 +0100

    modifica layout

--- TRIP_AGENCY/prenotazioni.php
+++ TRIP_AGENCY/prenotazioni.php
@@ -1,63 +1,179 @@
-<?php include 'header.php'; ?>
-<?php include 'db.php'; ?>
+<?php 
+    include 'header.php'; 
+    include 'db.php'; 
+
+
+    //Logica per impaginazione
+    $perPagina = 10;  // n elementi mostrati per pagina
+    $page = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+    $offset = ($page - 1) * $perPagina;
+
+
+
+
+
+    //LOGICA DI AGGIUNTA
+    //chiamata POST che prende il gancio del bottone aggiugi del form, prendendo i valori inseriti nei vari campi
+    if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['aggiungi'])){
+
+        //Preparo lo stato stmt -> statement 
+        $stmt = $conn->prepare("INSERT INTO prenotazioni (id_cliente, id_destinazione, dataprenotazione, acconto, numero_persone, assicurazione) 
+                                VALUES  (?, ?, ?, ?, ?, ?)");
+        //Binding dei parametri e tipizzo
+        $stmt->bind_param("iisiii", $_POST['id_cliente'], $_POST['id_destinazione'], $_POST['dataprenotazione'],$_POST['acconto'], $_POST['numero_persone'], $_POST['assicurazione']);
+        
+        //eseguo lo statement
+        $stmt->execute();
+
+        echo "<div class='alert alert-success'>Prenotazione Aggiunta!</div>";
+
+
+    }
+
+
+
+
+
+    //LOGICA DI MODIFICA
+    $prenotazione_modifica = null;
+
+    if (isset($_GET['modifica'])){
+
+
+        $res = $conn->query("SELECT * FROM prenotazioni WHERE id = " . intval($_GET['modifica']));
+
+        $prenotazione_modifica = $res->fetch_assoc();
+
+    }
+
+
+
+
+
+    //MODIFICA DEL DATO, SALVATAGGIO 
+    if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['salva_modifica'])){
+
+        //PREPARE
+        $stmt = $conn->prepare("UPDATE prenotazioni SET id_cliente=?, id_destinazione=?, dataprenotazione=?, acconto=?, numero_persone=?, assicurazione=? WHERE id=?");
+        //BINDING
+        $stmt->bind_param("iisiiii" ,$_POST['id_cliente'],$_POST['id_destinazione'],$_POST['dataprenotazione'],$_POST['acconto'],$_POST['numero_persone'],$_POST['assicurazione'],$_POST['id']);
+        //ESECUZIONE QUERY
+        $stmt->execute();
+        //messaggio
+        echo "<div class='alert alert-info'>Prenotazione Modificata correttamente</div>";
+    }
+
+
+
+
+
+    //CANCELLAZIONE CLIENTE
+    if(isset($_GET['elimina'])){
+
+        $id = intval($_GET['elimina']);
+        $conn->query("DELETE FROM prenotazioni WHERE id = $id");
+
+        echo "<div class='alert alert-info'>Prenotazione Cancellata correttamente</div>";
+    }
+
+    
+ ?>
+
+
+
 
 
 <h2>Prenotazioni</h2>
 
     <!--Form-->
     <div class="card mb-4">
-
         <div class="card-body">
+
 
             <form action="" method="POST">
 
-           
-
                 <div class="row g-3">
                     
-                    <div class="col-md-6">
-                        <label style="font-weight: 600;" for="">Seleziona Cliente : </label>
-                        <input type="text" name="cliente" class="form-control" placeholder="es.: Mario" 
+                    <div class="col-md-3">
+
+                        <label style="font-weight: 600;" for="">Cliente: </label>
+                        <select name="id_cliente" class="form-select" required>
+
+                            <option value="">Seleziona il cliente</option>
+
+
+
+                        </select>
+                        
+                    </div>
+                    
+                    <div class="col-md-3">
+                        <label style="font-weight: 600;" for="">Destinazione: </label>
+                        <select name="id_destinazione" class="form-select" required>
+
+                            <option value="">Seleziona Destinazione</option>
+
+                        </select>
+
+
+
+                    </div>
+                    
+                   
+                    
+                    <div class="col-md-3">
+                        <label style="font-weight: 600;" for="">Data Prenotazione: </label>
+                        <input type="date" name="dataprenotazione" class="form-control" placeholder="" 
+                        
+                        value="<?= $prenotazione_modifica['dataprenotazione'] ?? ''?>"
+                        
                         required>
                     </div>
-                    
-                    <div class="col-md-6">
-                        <label style="font-weight: 600;" for="">Seleziona Destinazione : </label>
-                        <input type="text" name="destinazione" class="form-control" placeholder="es.: Milano"
+
+                     <div class="col-md-3">
+                        <label style="font-weight: 600;" for="">Acconto: </label>
+                        <input type="number" name="acconto" class="form-control" placeholder="" 
+                        
+                        value="<?= $prenotazione_modifica['acconto'] ?? ''?>"
+                        
                         required>
                     </div>
+
+                   
                     
-                    <div class="col-md-6">
-                        <label style="font-weight: 600;" for="">Data Prenotazione : </label>
-                        <input type="date" name="dataprenotazione" class="form-control" placeholder="es: 01/01/2020" 
-                    required>
-                    </div>
-                    
-                    <div class="col-md-6">
+                    <div class="col-md-2">
                         <label style="font-weight: 600;" for="">Numero persone : </label>
-                        <input type="number" name="numero_persone" class="form-control" placeholder="es.: 10" 
+                        <input type="number" min ="1" name="numero_persone" class="form-control" placeholder="" 
+                        
+                        value="<?= $prenotazione_modifica['numero_persone'] ?? ''?>"
+                        
                         required>
                     </div>
                     
-                    <div class="col-md-6">
-                        <label style="font-weight: 600;" for="">Assicurazione : </label>
-                        <input type="checkbox" name="assicurazione"  placeholder="Assicurazione del cliente..." 
-                       
-                        required>
+
+                    
+                    <div class="col-md-2">
+                        <label style="font-weight: 600;" for="">Assicurazione: </label>
+                        
+                        <!--Logica ternaria dato assicurazione booleano/ tinyInt su Mysql trattato come int in php-->
+                        <input type="checkbox"  name="assicurazione" 
+                        
+                        
+                        value="1" <?= ($prenotazione_modifica['assicurazione'] ?? 0) ? 'checked' : ''?>  required>
+                        
+                      
                     </div>
                     
-                    <div class="col-md-6">
-                        <label style="font-weight: 600;" for="">Acconto : </label>
-                        <input type="number" name="acconto" class="form-control" placeholder="Acconto del cliente..." 
-                 
-                        required>
-                    </div>
                     
-                    <div class="col-12">
-                        <button name="<?= $prenotazione_modifica ? 'salva_modifica' : 'aggiungi' ?>" class="<?= $prenotazione_modifica ? 'warning' : 'success' ?>" 
-                        type="submit">
-                        <?= $prenotazione_modifica? 'Salva' : 'Aggiungi' ?>
+                    <div class="col-md-12">
+                        
+                        <button 
+                            name="<?= $prenotazione_modifica ? 'salva_modifica' : 'aggiungi' ?>" 
+                            class="<?= $prenotazione_modifica ? 'warning' : 'success' ?>" 
+                            type="submit">
+                            <?= $prenotazione_modifica ? 'Salva' : 'Aggiungi' ?>
                         </button>
+                    
                     </div>
 
                 </div>
@@ -65,8 +181,20 @@
         </div>
     </div>
 
-        
-  
+
+
+    <!--LOGICA RENDER -->
+    <?php
+
+        //vado a conteggiare il totale dei clienti con query
+        $total = $conn->query("SELECT COUNT(*) as t FROM prenotazioni")->fetch_assoc()['t'];
+        $totalPages = ceil($total / $perPagina); // il numero di pagine della navigazione
+
+        //QUERY PER ordinare i dati in modo DECRESCENTE IMPAGINATI PER valore di "$perPagina" 
+        $result = $conn->query("SELECT * FROM prenotazioni ORDER BY id ASC LIMIT $perPagina OFFSET $offset");
+
+    ?>
+
 
 
 
@@ -75,38 +203,68 @@
     <table class="table table-striped">
 
         <thead>
-
+            <!--Intestazione tabella-->
             <tr>
 
                 <th>ID</th>
                 <th>Cliente</th>
                 <th>Destinazione</th>
                 <th>Data</th>
-                <th>Persone</th>
-                <th>Assicurazione</th>
                 <th>Acconto</th>
+                <th>N.Persone</th>
+                <th>Assicurazione</th>
                 <th>Azioni</th>
 
             </tr>
 
         </thead>
-
+        <!--Corpo tabella-->
         <tbody>
 
+            <?php while ($row = $result->fetch_assoc()) : ?>
+                
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['nome'] ?><?= $row['cognome'] ?></td>
+                    <td><?= $row['citta'] ?></td>
+                    <td><?= $row['dataprenotazione'] ?></td>
+                    <td><?= $row['acconto'] ?></td>
+                    <td><?= $row['numero_persone'] ?></td>
+                    <td><?= $row['assicurazione'] ?></td>
+                    <td>
+
+                        <a class="btn btn-sm btn-warning" href="?modifica=<?= $row['id']  ?>">Modifica</a>
+                        <a class="btn btn-sm btn-danger" href="?elimina=<?= $row['id']  ?>" onclick="return confirm ('Sicuro?')">Elimina</a>
+
+
+                    </td>
+                </tr>
+
+
+            <?php endwhile; ?>
 
         </tbody>
-
 
     </table>
 
 
 
+    <!--Paginazione-->
+    <nav>
+
+        <ul class="pagination">
+
+            <?php for($i = 1; $i <= $totalPages; $i++ ) : ?>
+
+                <li class="page-item <?= $i == $page ? 'active' : '' ?>">
+                    <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                </li>   
+
+            <?php endfor; ?>
 
 
 
-
-
-
-
+        </ul>
+    </nav>
 
 <?php include 'footer.php'; ?>
