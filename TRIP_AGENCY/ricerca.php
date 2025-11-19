665a791943de8336fb5353a93158d784e5d15837
chiaratanzi00 <chiaratanzi00@gmail.com>
2025-11-19 15:33:39 +0100

    ricerca

--- TRIP_AGENCY/ricerca.php
+++ TRIP_AGENCY/ricerca.php
@@ -1,5 +1,61 @@
-<?php  include 'header.php' ?>
+<?php include 'header.php'; ?>
+<?php include 'db.php'; ?>
 
-<?php  include 'db.php' ?>
+
+
+    <h1 class="mb-4">Ricerca Destinazioni</h1>
+
+    <!-- Form di ricerca -->
+    <div class="card shadow-sm mb-4">
+        
+        <div class="card-body">
+            
+            <form method="GET" action="">
+                <div class="row g-3">
+
+                    <div class="col-md-3">
+                        <label class="form-label font-weight: 600">Città</label>
+                        <input type="text" name="citta" class="form-control"  placeholder="es. Milano">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label class="form-label font-weight: 600">Paese</label>
+                        <input type="text" name="paese" class="form-control"  placeholder="es. Italia">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label class="form-label font-weight: 600">Data Partenza</label>
+                        <input type="date" name="data_partenza" class="form-control">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label class="form-label font-weight: 600">Data Ritorno</label>
+                        <input type="date" name="data_ritorno" class="form-control">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label class="form-label font-weight: 600">Prezzo Minimo</label>
+                        <input type="number" name="prezzo_minimo" min="0" class="form-control">
+                    </div>
+
+                    <div class="col-md-3">
+                        <label class="form-label font-weight: 600">Prezzo Massimo</label>
+                        <input type="number" name="prezzo_massimo" min="0" class="form-control">
+                    </div>
+
+                    <div class="col-md-3">
+                    <label class="form-label font-weight: 600">Posti Disponibili</label>
+                    <input type="number" name="posti" min="1" class="form-control">
+                    </div>
+
+                    <div class="col-12 mt-3">
+                    <button type="submit" class="btn btn-primary">Cerca</button>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+
 
 <?php  include 'footer.php' ?>
